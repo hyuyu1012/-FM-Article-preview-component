68e963b563caaf248d2181509c4a0802df1e058d
hyuyu1012 <hanhyeongu495@gmail.com>
2025-06-25 19:35:35 +0900

    fontawsoem icon 삽입 완료

---
+++ script.js
@@ -0,0 +1,8 @@
+const shareButton = document.querySelector(".share-button");
+
+const shareIcon = document.querySelector(".fa-share");
+
+shareButton.addEventListener("click", () => {
+  shareButton.style.backgroundColor = "var(--Very-Dark-Grayish-Blue)"
+  shareIcon.style.color="#ffffff"
+})
